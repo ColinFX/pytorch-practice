c7eb50c5dc19820c9088299248732ba0ab1e4541
ColinFX <xiao.fei@polytechnique.edu>
2022-08-25 16:35:43 +0200

    Add core stage of train and add RunningAverage to utils

--- housing-linear/train.py
+++ housing-linear/train.py
@@ -9,11 +9,34 @@ import utils
 
 
 def train(model: nn.Module, 
-          optimizer: torch.optim, 
+          optimizer: torch.optim.optimizer.Optimizer, 
           loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.FloatTensor], 
           data_iterator: Generator[tuple(torch.Tensor, torch.Tensor), None, None], 
           metrics: dict[str, Callable[[np.ndarray, np.ndarray], float]],
           params: utils.Params, 
           num_steps: int):
+    """
+    Train the model on `num_steps` batches/iterations of size `params.batch_size` as one epoch. 
     
-    pass
+    Args:
+        * model: (nn.Module) the neural network
+        * optimizer: (torch.optim) the optimizer for parameters in the model
+        * loss_fn: (Callable) output_batch, labels_batch -> loss
+        * data_ietrator: (Generator) -> train_batch, labels_batch
+        * metrics: (dict) metric_name -> (function (Callable) output_batch, labels_batch -> metric_value)
+        * params: (utils.Params) hyperparameters
+        * num_steps: (int) number of batches to train for each epoch
+    """
+
+    model.train()
+    summ: List[dict[str, float]] = []
+    loss_avg = utils.RunningAverage()
+
+    t = trange(num_steps)
+    for i in t:
+        train_batch, targets_batch = next(data_iterator)
+        predicts_batch = model(train_batch)
+        loss = loss_fn(predicts_batch, targets_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
--- housing-linear/utils.py
+++ housing-linear/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 
@@ -28,3 +29,47 @@ class Params(object):
     def dict(self):
         """dict-like access to param by `params.dict["learning_rate"]`"""
         return self.__dict__
+
+
+class RunningAverage():
+    """
+    Examples:
+        * create running_avg instance by `running_avg = RunningAverage()`
+        * add new item by `running_avg.update(2.0)`
+        * get current average by `running_avg()`
+    """
+
+    def __init__(self):
+        self.steps: int = 0
+        self.total: float = 0
+
+    def udpate(self, val: float):
+        self.steps += 1
+        self.total += val
+
+    def __call__(self) -> float:
+        return self.total / float(self.steps)
+
+
+def set_logger(log_path: str):
+    """
+    Args:
+        log_path: (str) to store log files
+
+    Examples:
+        * write log by `logging.info("Start training...")`
+    """
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # logging to the file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
