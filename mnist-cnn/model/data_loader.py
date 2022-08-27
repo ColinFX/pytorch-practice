e0e22579ce427b9c00e1ac57dad49e790e43e180
ColinFX <xiao.fei@polytechnique.edu>
2022-08-27 21:02:50 +0200

    Remove unused import

--- mnist-cnn/model/data_loader.py
+++ mnist-cnn/model/data_loader.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
